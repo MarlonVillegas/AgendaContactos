15c576c4ba3dcc0418e0edef0220665b21c629b0
andrespiedrahita <bapiedrahita3@misena.edu.co>
2017-06-06 16:20:45 -0500

    Proyecto Biblioteca. Ya se implemento el mildware.

--- www/app/app.config.js
+++ www/app/app.config.js
@@ -1,25 +1,48 @@
 angular.module('AgendaContactos').constant('rolAdmin', 1);
 angular.module('AgendaContactos').constant('rolCelador', 2);
 
+/**
+ * middleware que comprueba las session y los tipos de roles
+ */
+angular.module('AgendaContactos').config(['$middlewareProvider', function middlewareProviderConfig($middlewareProvider) {
+        $middlewareProvider.map({
+            'comprobarSession': ['$sessionStorage', function comprobarSession($sessionStorage) {
+                    middlewareComprobarSession(this, $sessionStorage);
+                }],
+            'comprobarNoTenerSession': ['$sessionStorage', function comprobarNoTenerSession($sessionStorage) {
+                    middlewareComprobarNoTenerSession(this, $sessionStorage);
+                }]
+        });
+    }]);
 
 angular.module('AgendaContactos').config(['$routeProvider', '$httpProvider', function config($routeProvider, $httpProvider) {
         $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
         $routeProvider.
                 when('/', {
                     controller: 'loginController',
-                    templateUrl: 'app/template/login.html'
+                    templateUrl: 'app/template/login.html',
+                    middleware: ['comprobarNoTenerSession']
                 }).
                 when('/inicio', {
                     controller: 'inicioController',
-                    templateUrl: 'app/template/inicio.html'
+                    templateUrl: 'app/template/inicio.html',
+                    middleware: ['comprobarSession']
                 }).
                 when('/agregar', {
                     controller: 'agregarController',
-                    templateUrl: 'app/template/agregar.html'
+                    templateUrl: 'app/template/agregar.html',
+                    middleware: ['comprobarSession']
                 }).
                 when('/editar', {
                     controller: 'editarController',
-                    templateUrl: 'app/template/editar.html'
+                    templateUrl: 'app/template/editar.html',
+                    middleware: ['comprobarSession']
+                }).
+                when('/logout', {
+                    controller: 'logoutController',
+                    template: '<p>Cerrando sesión...</p>',
+                    middleware: ['comprobarSession']
+                    
                 }).
                 otherwise('/');
     }
--- www/app/controller/controller.editar.js
+++ www/app/controller/controller.editar.js
@@ -1,4 +1,38 @@
-angular.module('AgendaContactos').controller('editarController', ['$scope', function ($scope) {
+angular.module('AgendaContactos').controller('editarController', ['$scope', 'editarContactoService', '$sessionStorage', '$location', 'rolAdmin', '$route', '$timeout', function ($scope, editarContacto, $sessionStorage, $location, rolAdmin, $route, $timeout) {
+
+    $scope.editar = function (dato) {
+            $scope.edit.id = dato.con_id;
+            $scope.edit.nombres = dato.con_nombre;
+            $scope.edit.apellidos = dato.con_apellido;
+            $scope.edit.telefono = dato.con_telefono;
+            $scope.edit.correo = dato.con_correo;
+            $('#editarContacto').modal('toggle');
+        };
+
+        $scope.submitEditarContacto = function () {
+            editarContacto.editarCon($scope.edit).then(function successCallback(response) {
+                $scope.contactoEditado = false;
+                $scope.edit = {};
+                if (response.data.code == 500) {
+                } else {
+                    $scope.contactoEditado = true;
+                    $scope.edit = '';
+//                    $timeout(function () {
+//                        $('#editarContacto').modal('toggle');
+//                    }, 700);
+                    $timeout(function () {
+                        // $route.reload();
+                       // window.location.reload();
+                    }, 1000);
+                }
+            }, function errorCallback(response) {
+                console.error(response);
+            });
+        };
+
+
+
+
 
             }]);
 
