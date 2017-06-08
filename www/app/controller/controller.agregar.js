20ee1dfa0f7cd86c418581431893c5e7dc221a7d
MarlonVillegas <vargaz98@hotmail.com>
2017-06-08 15:45:57 -0500

    AgendaContactos: Preparacion para el examen php Ya funciona login, elimina y edita

--- www/app/controller/controller.agregar.js
+++ www/app/controller/controller.agregar.js
@@ -1,37 +1,38 @@
 angular.module('AgendaContactos').controller('agregarController', ['$scope', 'registroContactoService', '$sessionStorage', '$location', 'rolAdmin', '$route', '$timeout', function ($scope, agregarContacto, $sessionStorage, $location, rolAdmin, $route, $timeout) {
-        $scope.dataRegistrarContacto = {
-            foto: '',
-            nombre: '',
-            apellido: '',
-            telefono: '',
-            correo: ''
-        };
+    $scope.dataRegistrarContacto = {
+      foto: '',
+      nombre: '',
+      apellido: '',
+      telefono: '',
+      correo: ''
+    };
+    $scope.modeloSoloLetras = '^[a-zA-Z ]+$';
+    $scope.contactoRegistrado = false;
+
+    $scope.submitNuevoContacto = function () {
+      agregarContacto.agregarCon($scope.dataRegistrarContacto).then(function successCallback(response) {
         $scope.contactoRegistrado = false;
-
-        $scope.submitNuevoContacto = function () {
-            agregarContacto.agregarCon($scope.dataRegistrarContacto).then(function successCallback(response) {
-                $scope.contactoRegistrado = false;
-                $scope.dataRegistrarContacto = {};
-                if (response.data.code == 500) {
-                } else {
-                    $scope.contactoRegistrado = true;
-                    $scope.dataRegistrarContacto = '';
-//                    $timeout(function () {
-//                        $('#nuevoContacto').modal('toggle');
-//                    }, 700);
-                    $timeout(function () {
-                        // $route.reload();
-                        //window.location.reload();
-                        $location.path('inicio');
-                    }, 1000);
-                }
-            }, function errorCallback(response) {
-                console.error(response);
-            });
-        };
+        $scope.dataRegistrarContacto = {};
+        if (response.data.code == 500) {
+        } else {
+          $scope.contactoRegistrado = true;
+          $scope.dataRegistrarContacto = '';
+          $timeout(function () {
+            $('#nuevoContacto').modal('toggle');
+          }, 700);
+          $timeout(function () {
+            // $route.reload();
+            window.location.reload();
+            $location.path('inicio');
+          }, 1000);
+        }
+      }, function errorCallback(response) {
+        console.error(response);
+      });
+    };
 
 
-    }]);
+  }]);
 
 
 
